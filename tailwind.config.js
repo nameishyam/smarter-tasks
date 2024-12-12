528c53d1f46839745054f795dcd9a9a0ea34fbea
Geddam Shyam Gowtham <geddamgowtham04@outlook.com>
2024-12-12 08:35:59 +0530

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+export const content = {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
