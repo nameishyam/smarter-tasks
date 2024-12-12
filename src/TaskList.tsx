bf33ea987cf2ccbef8675112b67c903364701433
Geddam Shyam Gowtham <geddamgowtham04@outlook.com>
2024-12-12 14:38:23 +0530

    refactor: remove App.css and implement TaskList component with task state management

--- src/TaskList.tsx
+++ src/TaskList.tsx
@@ -1,12 +1,41 @@
 import React from "react";
 import Task from "./Task";
 
-class TaskList extends React.Component {
+type Props = object;
+
+interface TaskItem {
+  title: string;
+}
+
+interface State {
+  tasks: TaskItem[];
+}
+
+class TaskList extends React.Component<Props, State> {
+  constructor(props: Props) {
+    super(props);
+    this.state = {
+      tasks: [
+        { title: "Task 1" },
+        { title: "Task 2" },
+        { title: "Task 3" },
+        { title: "Task 4" },
+        { title: "Task 5" },
+        { title: "Task 6" },
+        { title: "Task 7" },
+        { title: "Task 8" },
+        { title: "Task 9" },
+        { title: "Task 10" },
+      ],
+    };
+  }
   render() {
     return (
-      <div>
-        <Task title="gowtham" />
-      </div>
+      <>
+        {this.state.tasks.map((task) => (
+          <Task title={task.title} />
+        ))}
+      </>
     );
   }
 }
