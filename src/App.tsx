e1c68ccf602198822044643754dfb16b7e82d2b7
Geddam Shyam Gowtham <geddamgowtham04@outlook.com>
2024-12-24 06:30:47 +0530

    feat: implement layout component with header and nested routing for HomePage and TaskListPage

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,21 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import TaskListPage from "./pages/TaskListPage";
+import Layout from "./Layout";
 
 const router = createBrowserRouter([
   {
-    path: "/",
-    element: <HomePage />,
-  },
-  {
-    path: "/tasks",
-    element: <TaskListPage />,
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <HomePage />,
+      },
+      {
+        path: "tasks",
+        element: <TaskListPage />,
+      },
+    ],
   },
 ]);
 
